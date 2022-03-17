bd6838cf891c3f209ff4b8177d12ba13125c7df4
jannb-swiss <jan.nussberger@outlook.com>
2022-03-17 10:48:12 +0100

    add layout for login and register

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,14 +1,14 @@
-<div class="container-footer">
+<div class="container-box">
     <footer class="py-3 mt-2">
         <ul class="nav justify-content-center border-bottom-footer ps-3 mb-3">
             <li class="nav-item">
-                <a href="/home" class="nav-link px-2 text-footer">home</a>
+                <a href="/home" class="nav-link px-2 footer-font">home</a>
             </li>
             <li class="nav-item">
-                <a href="/about" class="nav-link px-2 text-footer">about</a>
+                <a href="/about" class="nav-link px-2 footer-font">about</a>
             </li>
             <li class="nav-item">
-                <a href="/login" class="nav-link px-2 text-footer">login</a>
+                <a href="/login" class="nav-link px-2 footer-font">login</a>
             </li>
         </ul>
         <p class="text-center">HappyLife 2022</p>
