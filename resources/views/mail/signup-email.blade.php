f159d52ce86c2d9529a88482d9827f4b12a21fd9
jannb-swiss <jan.nussberger@outlook.com>
2022-05-31 09:10:26 +0200

    add new link

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -1,6 +1,6 @@
 Hello {{$email_data['name']}}
 <br>
 Thanks for your registration!
-<a href="http://127.0.0.1:8000/verify?code={{$email_data['verification_code']}}">Jetzt bestätigen!</a>
+<a href="http://68.183.1.72/verify?code={{$email_data['verification_code']}}">Jetzt bestätigen!</a>
 <br>
 Thank you!
