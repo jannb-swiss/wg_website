a7d15c9cf198acc303a49ab83ad6c95caaa82814
jannb-swiss <jan.nussberger@outlook.com>
2022-06-06 20:47:47 +0200

    change link

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -1,7 +1,7 @@
 Hallo {{$email_data['name']}}
 <br>
 Drücke auf den Link, um deinen Account zu bestätigen
-<a href="http://127.0.0.1:8000/verify?code={{$email_data['verification_code']}}">Jetzt bestätigen!(for testing!)</a>
-{{--<a href="https://www.wg-website.com/verify?code={{$email_data['verification_code']}}">Jetzt bestätigen!</a>--}}
+<!--<a href="http://127.0.0.1:8000/verify?code={{$email_data['verification_code']}}">Jetzt bestätigen!(for testing!)</a>-->
+<a href="https://www.wg-website.com/verify?code={{$email_data['verification_code']}}">Jetzt bestätigen!</a>
 <br>
 Vielen Dank!
