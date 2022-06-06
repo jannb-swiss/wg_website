931bb45e8a6db3744356e1c66ceb7327e4c31fad
jannb-swiss <jan.nussberger@outlook.com>
2022-06-06 20:42:16 +0200

    add new chat design

--- resources/views/chat/indexChat.blade.php
+++ resources/views/chat/indexChat.blade.php
@@ -1,20 +1,33 @@
 @extends('layouts.app')
+
 @section('content')
-    <div class="container mt-5">
-        <div class="card">
-            <div class="card-header">Chats</div>
-            <div class="card-body">
-                <chat-messages :messages="messages"></chat-messages>
-            </div>
-            <div class="card-footer">
-                <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
+
+    @include('layouts.hero', [
+'heroTitle' => 'Happy Living',
+'heroSubtitle' => 'Make living great again',
+    'heroImage' => '/images/img_phone_2.jpg',
+    'heroImageTitle' => 'WG-Website'
+])
+    <section class="container-lg mt-3">
+        <div class="">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Nachrichten</div>
+                        <div class="card-body">
+                            <chat-messages :messages="messages"></chat-messages>
+                        </div>
+                        <div class="card-footer">
+                            <chat-form v-on:messagesent="addMessage" :user="{{ Auth::user() }}"></chat-form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div>
-        @include('components.footer')
-    </div>
+    </section>
+        <div>
+            @include('components.footer')
+        </div>
 
 @endsection
 
